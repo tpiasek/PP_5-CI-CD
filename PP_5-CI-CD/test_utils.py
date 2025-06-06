baccd6da78d0eb505e4348eccff9b428952e0bdf
tpiasek <tpiasekk@gmail.com>
2025-06-06 07:54:53 +0200

    Fixes to the yaml file.

--- PP_5-CI-CD/test_utils.py
+++ PP_5-CI-CD/test_utils.py
@@ -10,8 +10,8 @@ def test_add(a, b, expected):
     assert result == expected
 
 @pytest.mark.parametrize("a, b, expected ", [(1, 2, -1), (2, 3, -1), (3, 4, -1), (4, 5, -1)])
-def test_substract(a, b, expected):
-    result = utils.substract(a, b)
+def test_subtract(a, b, expected):
+    result = utils.subtract(a, b)
     assert result == expected
 
 @pytest.mark.parametrize("a, b, expected ", [(1, 2, 2), (2, 3, 6), (3, 4, 12), (4, 5, 20)])
